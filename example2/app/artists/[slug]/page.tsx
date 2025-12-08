d76a7bb29304614988916f72da674d2482817df5
Emerson do Nascimento Rodrigues <emersondonascimentorodrigues@gmail.com>
2025-12-08 14:17:00 -0300

    rm example

--- example2/app/artists/[slug]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function ArtistSlugRedirect({ params }: { params: { slug: string } }) {
-  redirect(`/talent/${params.slug}`);
-}
--- example2/app/artists/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function ArtistsRedirect() {
-  redirect('/talent');
-}
